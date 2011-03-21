520f3cc72b90bba9c0c42964e9784177086a8f6b
slingshot316 <selwynjacob90@gmail.com>
2011-03-21 10:23:35 +0530

    wrote Snippet model

--- app/models.py
+++ app/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-from pygments import lexers
+from pygments import formatters, highlight, lexers
+from tagging.fields import TagField
+from markdown import markdown
+import datetime
 
 class Language(models.Model):
     name = models.CharField(max_length=100)
@@ -20,4 +24,31 @@ class Language(models.Model):
 
     def get_lexer(self):
         return lexers.get_lexer_by_name(self.language_code)
+
+
+class Snippet(models.Model):
+    title = models.CharField(max_length=255)
+    language = models.ForeignKey(Language)
+    author = models.ForeignKey(User)
+    description = models.TextField()
+    description_html = models.TextField(editable=False)
+    code = models.TextField()
+    highlighted_code = models.TextField(editable=False)
+    tags = TagField()
+    pub_date = models.DateTimeField(editable=False)
+    updated_date = models.DateTimeField(editable=False)
+
+    class Meta:
+        ordering = ['-pub_date']
+
+    def __unicode__(self):
+        return self.title
+
+    def save(self, force_insert=False, force_update=False):
+        if not self.id:
+            self.pub_date = datetime.datetime.now()
+        self.updated_date = datetime.datetime.now()
+        self.description_html = markdown(self.description)
+        self.highlighted_code = self.highlight()
+        super(Snippet, self).save(force_insert, force_update)
 
