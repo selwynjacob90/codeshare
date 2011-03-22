8ad7e3dde6f43308fa8f1f209009c92acf5bae5b
slingshot316 <selwynjacob90@gmail.com>
2011-03-22 16:02:41 +0530

    wrote views for language model

---
+++ app/views/languages.py
@@ -0,0 +1,14 @@
+from django.shortcuts import get_object_or_404
+from django.views.generic.list_detail import object_list
+
+from app.models import Language
+
+
+def language_detail(request, slug):
+    language = get_object_or_404(Language, slug=slug)
+    return object_list(request,
+                       queryset=language.snippet_set.all(),
+                       paginate_by=20,
+                       template_name='app/language_detail.html',
+                       extra_content = { 'language' : language })
+
