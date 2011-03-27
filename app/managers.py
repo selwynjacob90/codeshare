11be6fae46dc48ae1950510e89b053937c26ad6a
slingshot316 <selwynjacob90@gmail.com>
2011-03-27 21:20:06 +0530

    wrote custom managers for Language and Snippet models

---
+++ app/managers.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Count
+
+
+class SnippetManager(models.Manager):
+    """Custom manager for Snippet Model
+    """
+    
+    def top_authors(self):
+        return User.objects.annotate(score=Count('snippet')).order_by('score')
+
+class LanguageManager(models.Manager):
+    """Custom Manager for Language Model
+    """
+
+    def top_languages(self):
+        return self.annotate(score=Count('snippet')).order_by('score')
+
+
