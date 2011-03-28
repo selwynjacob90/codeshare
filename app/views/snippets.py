716e9273e5647f0879ceda4de706d73b26dd8d03
slingshot316 <selwynjacob90@gmail.com>
2011-03-28 16:13:44 +0530

    renamed views.snippet..py to snippet.py

--- app/views/snippets.py
+++ app/views/snippets.py
@@ -2,20 +2,22 @@ from django.http import HttpResponseRedirect
 from django.forms import ModelForm
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 
 from app.models import Snippet
 
 
 class SnippetForm(ModelForm):
-"""Automatically generate form from Snippet Model.
-"""
+    """Automatically generate form from Snippet Model.
+    """
     class Meta:
         model = Snippet
         exclude = ['author']
-    
+
+@csrf_exempt    
 def add_snippet(request):
-"""view to add a snippet.
-"""
+    """view to add a snippet.
+    """
     if request.method == 'POST':
         form = SnippetForm(data=request.POST)
         if form.is_valid():
@@ -27,8 +29,7 @@ def add_snippet(request):
         form = SnippetForm()
     return render_to_response('app/snippet_form.html', 
                               { 'form': form, 'add': True },
-                              context_instance=RequestContext(request))
-
+                             )   
 add_snippet = login_required(add_snippet)
 
 
