18ab223b630163b1c36465306f90723e176eed11
slingshot316 <selwynjacob90@gmail.com>
2011-03-28 10:29:15 +0530

    wrote a view to list top languages

--- app/views/popular.py
+++ app/views/popular.py
@@ -3,9 +3,21 @@ from django.views.generic.list_detail import object_list
 
 from django.db.models import Count
 
+from app.models import Language, Snippet
+
+
 def top_authors(request):
+    """ view to return the top authors
+    """
     return object_list(request, queryset=Snippet.objects.top_authors(),
                        template_name='app/top_authors.html',
+                       paginate_by=20)
+
+def top_languages(request):
+    """ view to return the top languages
+    """
+    return object_list(request, queryset=Language.objects.top_languages(),
+                       template_name='app/top_languages.html',
                        paginate_by=20)
 
 
