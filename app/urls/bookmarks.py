78b98a4e704a1f2cb104492dd9f9bb207b1547d8
slingshot316 <selwynjacob90@gmail.com>
2011-04-02 15:55:53 +0530

    urls for bookmark

---
+++ app/urls/bookmarks.py
@@ -0,0 +1,15 @@
+from django.conf.urls.defaults import *
+from app.views import bookmarks
+
+urlpatterns = patterns('',
+    url(r'^add/(?P<snippet_id>\d+)/$',
+        bookmarks.add_bookmark,
+        name='app_bookmark_add'),
+
+    url(r'^delete/(?P<snippet_id>\d+)/$',
+        bookmarks.delete_bookmark,
+        name='app_bookmark_delete'),
+    
+    url(r'^$', bookmarks.user_bookmarks, name='app_user_bookmarks'),
+)
+
