7c5db3b695415535f8844ed3093fabd3d4b046e2
slingshot316 <selwynjacob90@gmail.com>
2011-04-02 11:40:55 +0530

    fixed url for snippets

--- app/urls/snippets.py
+++ app/urls/snippets.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 from django.views.generic.list_detail import object_list, object_detail
 
 from app.models import Snippet
-from app.views.snippets import add_snippet
+from app.views.snippets import add_snippet, edit_snippet
 
 snippet_info = { 'queryset' : Snippet.objects.all() }
 
@@ -16,5 +16,12 @@ urlpatterns = patterns('',
                            object_detail,
                            snippet_info,
                            name = 'app_snippet_detail'),
-                       url(r'^add/$', add_snippet, name='app_snippet_add'),
+
+                       url(r'^add/$',
+                           add_snippet,
+                           name='app_snippet_add'),
+
+                       url(r'^edit/(?P<snippet_id>\d+)/$',
+                           edit_snippet,
+                           name='app_snippet_edit'),
                      )
